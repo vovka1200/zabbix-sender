d6a7f2872646b18ee445b971a2d6eed4f805d1e4
Vladimir Yavorskiy <vovka@krevedko.su>
2020-06-12 19:22:52 +0300

    added nanoseconds

--- converter.go
+++ converter.go
@@ -59,8 +59,9 @@ func (di DataItems) Marshal() (b []byte, err error) {
 	if err == nil {
 		// the order of fields in this "JSON" is important - request should be before data
 		now := fmt.Sprint(time.Now().Unix())
-		datalen := uint64(len(d) + len(now) + 42) // 32 + d + 9 + now + 1
-		b = make([]byte, 0, datalen+13)           // datalen + 5 + 8
+		ns := fmt.Sprint(time.Now().Nanosecond())
+		datalen := uint64(len(d) + len(now) + 42 + len(ns) + 6) // 32 + d + 9 + now + 6 + ns + 1
+		b = make([]byte, 0, datalen+13)                         // datalen + 5 + 8
 		buf := bytes.NewBuffer(b)
 		buf.Write(header)                                     // 5
 		err = binary.Write(buf, binary.LittleEndian, datalen) // 8
@@ -68,6 +69,8 @@ func (di DataItems) Marshal() (b []byte, err error) {
 		buf.Write(d)                                          // d
 		buf.WriteString(`,"clock":`)                          // 9
 		buf.WriteString(now)                                  // now
+		buf.WriteString(`,"ns":`)                             // 6
+		buf.WriteString(ns)                                   // ns
 		buf.WriteByte('}')                                    // 1
 		b = buf.Bytes()
 	}
